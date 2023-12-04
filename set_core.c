aa3d41b8c964e0145d69e01faa43d60bc7cf94ac
Jayanth-1090 <152853678+Jayanth-1090@users.noreply.github.com>
2023-12-04 14:29:56 +0530

    Create set_core.c

---
+++ set_core.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include<pthread.h>
+#include <sys/wait.h>
+#include <sys/prctl.h>
+
+
+void set_core_withPID(pid_t pid, int core_id) 
+{
+
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(core_id, &mask);
+
+    pthread_t current_thread = pthread_self();
+
+    if (sched_setaffinity(pid, sizeof(cpu_set_t), &mask) != 0) {
+        perror("pthread_setaffinity_np");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main()
+{
+    pid_t pid;
+    int core_id;
+    
+    printf("Input pid: ");
+    if (scanf("%d", &pid) != 1)
+    {
+        fprintf(stderr, "Error reading pid\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Input which core you need to run on: ");
+    if (scanf("%d", &core_id) != 1)
+    {
+        fprintf(stderr, "Error reading core_id\n");
+        exit(EXIT_FAILURE);
+    }
+
+    set_core_withPID(pid, core_id);
+    return 0;
+}
